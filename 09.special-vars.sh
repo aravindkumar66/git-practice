dba8a0577d59d7a4248b7887b3bc1aa82fe9193c
aravind kumar <aravindkumarreddy66@gmail.com>
2024-09-17 14:47:43 +0530

    functions

--- 09.special-vars.sh
+++ 09.special-vars.sh
@@ -6,4 +6,5 @@ echo "script name: $0"
 echo "current working direcctory: $PWD"
 echo "home directory of current user: $HOME"
 echo  "pid of the script executing now: $$"
+sleep 100 &
 echo  "pid of the last bagruound command: $!"
