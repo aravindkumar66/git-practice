aef49519e6f897a41381a017bfa18976c40617a9
aravind kumar <aravindkumarreddy66@gmail.com>
2024-09-17 19:57:39 +0530

    functions

--- 10-installation.sh
+++ 10-installation.sh
@@ -10,5 +10,13 @@ then
     exit 1
 fi
 
-dnf install git -y
+dnf list installed git 
+
+fi [ $? -ne 0 ]
+then
+    echo "git installed, going to install it"
+    dnf install git -y
+else
+    echo "git is already installed, nothing to do.."
+fi
 
