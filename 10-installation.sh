d04f3442927edd8229e74c518aaece90ddf5b676
aravind kumar <aravindkumarreddy66@gmail.com>
2024-09-17 20:20:52 +0530

    functions

--- 10-installation.sh
+++ 10-installation.sh
@@ -10,12 +10,19 @@ then
     exit 1
 fi
 
-dnf list installed git 
+dnf list installed gitttt
 
 if [ $? -ne 0 ]
 then
     echo "git installed, going to install it"
-    dnf install git -y
+    dnf install gitttt -y
+    if [ $? -ne 0 ]
+    then 
+        echo "git installation not sucess..."
+        exit 1
+    else
+        echo "git installation is sucess"
+    fi
 else
     echo "git is already installed, nothing to do.."
 fi
