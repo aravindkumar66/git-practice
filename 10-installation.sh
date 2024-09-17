be62d5a9d95db7a3e8e2e28d30a863b51b0129cf
aravind kumar <aravindkumarreddy66@gmail.com>
2024-09-17 19:47:12 +0530

    functions

--- 10-installation.sh
+++ 10-installation.sh
@@ -4,15 +4,11 @@
 # #echo "User ID is: $USERID"
 
 
-# if [ $USERID -ne 0 ]
-# then 
-#     echo "Please run this script with root priviliges"
-# fi
+if [ $USERID -ne 0 ]
+then 
+    echo "Please run this script with root priviliges"
+    exit 1
+fi
 
-# dnf install git -y
+dnf install git -y
 
-ls -ltr
-
-lsdsdg -ltr
-
-ls -l 
