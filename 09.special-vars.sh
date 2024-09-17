19d39d5b99a91f46e720348520289fb68040ddf3
aravind kumar <aravindkumarreddy66@gmail.com>
2024-09-17 14:33:03 +0530

    functions

--- 09.special-vars.sh
+++ 09.special-vars.sh
@@ -3,3 +3,5 @@
 echo "all variables passed to the script: $@"
 echo "number of variables passed: $#"
 echo "script name: $0"
+echo "current working direcctory: $PWD"
+echo "home directory of current user: $HOME"
