a459e7aea105be31d343bc903586ab087dd114c8
aravind kumar <aravindkumarreddy66@gmail.com>
2024-09-18 16:05:41 +0530

    functions

--- 13-loops.sh
+++ 13-loops.sh
@@ -4,7 +4,7 @@
 # #echo "User ID is: $USERID"
 
 CHECK_ROOT(){
-    if [ $USERID -NE 0 ]
+    if [ $USERID -ne 0 ]
     then
         echo "please run this script with root previliges"
         exit 1
